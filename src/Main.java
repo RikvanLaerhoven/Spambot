9696041ab5d67085ea80842d4a15ba19df114873
Rik van Laerhoven <rikvanlaerhoven@hotmail.com>
2019-02-06 12:35:00 +0100

    small changes

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         // amount is het aantal keer dat de text gestuurd wordt, text is de text die gestuurd moet worden.
-        Spam spam = new Spam(100,1000,"=");
+        Spam spam = new Spam();
         spam.start();
-
-    }
+        }
 }
